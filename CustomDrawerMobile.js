2bf2143d7f2a120411e39ff4b8fd8b137b4454c4
David <dbzx-10299@hotmail.com>
2021-07-09 13:44:56 +0200

    Update CustomDrawerMobile.js

--- CustomDrawerMobile.js
+++ CustomDrawerMobile.js
@@ -6,79 +6,75 @@ function CustomDrawerMobile() {
     }
     let template = '';
   
-	// pw--slide-right must be passed in due to css selector
+    // pw--slide-right must be passed in due to css selector
     function render(drawerType, contentModifier) {
-      if (drawerType) {
-        return `
-			<div class="pw-sheet ${drawerType}">
-                <div class="pw-sheet__mask"></div>
-                <div class="pw-sheet__wrapper ${contentModifier}">
-					<div class="pw-sheet__inner">
-                      <div class="pw-sheet__content">
-                          	
-                      </div>
-                    </div>
-                </div>
-            </div>`; 
-      }     
-          return `
-  			<div class="pw-sheet pw--slide-bottom">
-  				<div class="pw-sheet__mask"></div>
-  				<div class="pw-sheet__wrapper ${contentModifier}">
-  					<div class="pw-sheet__inner">
-  						<div class="pw-sheet__content">
+        if (drawerType) {
+		return `
+		    <div class="pw-sheet ${drawerType}">
+			<div class="pw-sheet__mask"></div>
+			<div class="pw-sheet__wrapper ${contentModifier}">
+			    <div class="pw-sheet__inner">
+				<div class="pw-sheet__content">
 
-  						</div>
-  					</div>
-  				</div>
-  			</div>`;
+				</div>
+			    </div>
+			</div>
+		    </div>`; 
+	      }     
+	      return `
+		  <div class="pw-sheet pw--slide-bottom">
+		      <div class="pw-sheet__mask"></div>
+			  <div class="pw-sheet__wrapper ${contentModifier}">
+			      <div class="pw-sheet__inner">
+				  <div class="pw-sheet__content">
+
+				  </div>
+			      </div>
+			  </div>
+		      </div>`;
     }
   
   
     // pw--slide-right should be only option to use
-	function open(drawerType, contentModifier) {
-       template = render(drawerType, contentModifier);
-       $('#main').append(template);
+    function open(drawerType, contentModifier) {
+        template = render(drawerType, contentModifier);
+        $('#main').append(template);
       
-      if (drawerType) {
-          setTimeout(function() {
-          $('.pw-sheet__mask').css("opacity", "0.5");
-          $('.pw-sheet__wrapper').css("transform", "translateX(0%)");
-        }); 
-      } else {
-          setTimeout(function() {
-          $('.pw-sheet__mask').css("opacity", "0.5");
-          $('.pw-sheet__wrapper').css("transform", "translateY(0%)");
-        });
-      }
+        if (drawerType) {
+            setTimeout(function() {
+                $('.pw-sheet__mask').css("opacity", "0.5");
+                $('.pw-sheet__wrapper').css("transform", "translateX(0%)");
+            }); 
+        } else {
+              setTimeout(function() {
+                  $('.pw-sheet__mask').css("opacity", "0.5");
+                  $('.pw-sheet__wrapper').css("transform", "translateY(0%)");
+              });
+        }
     }
-  
   
   
     function close() {
-      $('.pw-sheet__mask').css("opacity", "0");
-      $('.pw-sheet__wrapper').css("transform", "");
+        $('.pw-sheet__mask').css("opacity", "0");
+        $('.pw-sheet__wrapper').css("transform", "");
       
-      setTimeout(() => {
-		$(selector.drawer).remove();
-	  }, 400);
+        setTimeout(() => {
+	    $(selector.drawer).remove();
+	}, 400);
+    }
+
+    function closeImmediate() {
+        $(selector.drawer).remove();
     }
 
 
-	function closeImmediate() {
-    	$(selector.drawer).remove();
-    }
-
-
-
-	$(document).on('click', selector.close, function() {
-      	if ($(selector.close).hasClass('pw--close-immediate')) {
-        	closeImmediate();
+    $(document).on('click', selector.close, function() {
+        if ($(selector.close).hasClass('pw--close-immediate')) {
+            closeImmediate();
         } 
         close();
     })
   	
-
     return {
         selector,
         open,
